9dcd13746d6702a17b1f1938dd5bbdb53f7372ac
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2023-01-23 16:09:43 +0000

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Sunjay Kalsi on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
